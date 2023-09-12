44813661406afc89fb3b0d44849e45f08ff4767d
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-09-12 21:45:40 +0600

    handle events in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("button clicked");
+  }
+  const addToFive = (num) => alert(num + 5);
   return (
     <>
       <h3>React Core Concepts Part 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={() => {
+          alert("click me 2 clicked");
+        }}
+      >
+        Click Me 2
+      </button>
+      <button onClick={() => addToFive(3)}>Four</button>
     </>
   );
 }
